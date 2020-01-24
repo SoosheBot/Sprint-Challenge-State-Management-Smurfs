2eb1aac66feb8417494af043ceacba826ae4baff
sooshe <sooshe@sooshebot.com>
2020-01-24 10:47:06 -0600

    init import to index file, wrap app in Provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById("root"));
